6f7adc2a6afcd62a4b105634b8c8898bfb15ac17
YOUR NAME <jhanov@gmail.com>
2022-05-20 23:22:15 -0500

    rounded grades

---
+++ practice.rb
@@ -0,0 +1,25 @@
+#   ROUNDING GRADES
+# def gradingStudents(grades)
+#   final_grades = []
+#   grades.to_a
+#   i = 0
+#   while i < grades.length
+#       if grades[i] < 38
+#           final_grades << grades[i]
+#       else
+#           checker = grades[i]
+#           while checker % 5 != 0
+#               checker += 1
+#           end
+
+#          if checker - grades[i] < 3
+#              final_grades << checker
+#          else
+#           final_grades << grades[i]
+#          end
+#       end
+#       i += 1
+#   end
+
+#   return final_grades
+# end
