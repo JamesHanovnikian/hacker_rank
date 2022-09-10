27af2477353043e597580874019438a0642d6feb
YOUR NAME <jhanov@gmail.com>
2022-09-10 11:42:57 -0500

    kangaroo jump solution

--- practice.rb
+++ practice.rb
@@ -1174,3 +1174,15 @@
 #   end
 #   triplet_count
 # end
+
+# def kangaroo(x1, v1, x2, v2)
+#   if x2 > x1 && v2 > v1
+#        return "NO"
+#   elsif x1 > x2 && v1 > v2
+#        return "NO"
+#   elsif v1 != v2 && (x2-x1) % (v1-v2) == 0
+#         return "YES"
+#    else
+#         return "NO"
+#   end
+# end
